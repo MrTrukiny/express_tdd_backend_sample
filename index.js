f9d24ba29dc2222eb9b9dd923927b01f148afd20
MrTrukiny <carloseduardo.tkn@gmail.com>
2022-05-04 18:54:53 -0500

    Create mongodb connection with mongoose

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`.yellow.bold);
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+require('colors');
+
+// Config
+const connectToDb = require('./config/database');
 
 const app = express();
+connectToDb();
 
 app.post('/api/v1.0/auth/local/signup', (req, res) => {
   res.status(201).send({ message: 'Success' });
