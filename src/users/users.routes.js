cc02372e4eb9d75cc4ec0a034ed6cf97104bfa77
MrTrukiny <carloseduardo.tkn@gmail.com>
2022-05-16 19:07:44 -0500

    Install and use express-validator

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
-// const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 // Controllers
 const { postUser } = require('./controllers');
 
-// Middlewares
+/* // Middlewares
 const {
   validateEmail,
   validatePassword,
-} = require('../shared/middlewares/userValidation.middlewares');
+} = require('../shared/middlewares/userValidation.middlewares'); */
 
 const { API_BASE_URL } = require('../shared/constants');
 
@@ -17,8 +17,22 @@ const router = Router();
 
 router.post(
   `${API_BASE_URL}/auth/local/signup`,
-  validateEmail,
-  validatePassword,
+  body('email').notEmpty().withMessage('Email cannot be empty'),
+  body('password').notEmpty().withMessage('Password cannot be empty'),
+  (req, res, next) => {
+    // Finds the validation errors in this request
+    // and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    const validationErrors = {};
+    if (!errors.isEmpty()) {
+      errors.array().forEach((error) => {
+        validationErrors[error.param] = error.msg;
+      });
+      return res.status(400).send({ validationErrors });
+    }
+
+    next();
+  },
   postUser,
 );
 
